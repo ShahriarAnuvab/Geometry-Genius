5b1c8b0113d2f6f9e7380f13a031c88dcc803bd7
ShahriarAnuvab <113209361+ShahriarAnuvab@users.noreply.github.com>
2023-08-19 21:24:36 +0600

    3rd commit

--- Scripts/functions.js
+++ Scripts/functions.js
@@ -15,3 +15,14 @@ function setElementValue(elementId, newValue) {
   const element = document.getElementById(elementId);
   element.innerText = newValue;
 }
+
+function addCalculation(areaType, area){
+  console.log(areaType+ " " + area);
+  const addDiv = document.getElementById('addCalculation');
+  const p = document.createElement('p');
+  // p.innerText = areaType + " " + area;
+  p.classList.add('m-5', 'text-center')
+  const count =addDiv.childElementCount;
+  p.innerHTML = `${count+1}. ${areaType}: ${area}  <sup>cm</sup> <button class="btn btn-sm">Convert</button>`
+  addDiv.appendChild(p); 
+}
--- Scripts/index.js
+++ Scripts/index.js
@@ -4,10 +4,20 @@ document.getElementById('btn-Triangle').addEventListener('click', function(){
     const inputHeight = getInputValue('inputHeight');
     // console.log(inputBase);
     // console.log(inputHeight);
-    const triangle = 0.5 * inputBase * inputHeight;
-    console.log(triangle);
-    const areaValue = getElementValue('triangleArea')
-    console.log(areaValue);
-    setElementValue('triangleArea', triangle);
-    
+    if(isNaN(inputBase) || isNaN(inputHeight)){
+        alert('Invalid Input');
+    }else{
+        const area = 0.5 * inputBase * inputHeight;
+        console.log(area);
+        const areaValue = getElementValue('triangleArea')
+        console.log(areaValue);
+        setElementValue('triangleArea', area);
+        addCalculation('Triangle', area);        
+    }
+    // const area = 0.5 * inputBase * inputHeight;
+    // console.log(area);
+    // const areaValue = getElementValue('triangleArea')
+    // console.log(areaValue);
+    // setElementValue('triangleArea', area);
+    // addCalculation('Triangle', area);
 })
